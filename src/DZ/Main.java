9d798b64beb20a8a5e95dda7395527cd1dc05b21
OlgaTurkina <olechka967@rambler.ru>
2023-10-12 17:41:46 +0400

    DZ

--- src/DZ/Main.java
+++ src/DZ/Main.java
@@ -4,38 +4,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello! It`s a calc");
-        Scanner calc = new Scanner(System.in);
-        System.out.println("Input x - first number");
-        int x = calc.nextInt();
-        System.out.println("Input y - second number");
-        int y = calc.nextInt();
-        System.out.println("Input ~1~, if +, ~2~, if -, ~3~, if *, ~4~, if /");
-        int j = calc.nextInt();
-        System.out.printf("x = %d, y = %d, j = %d \n", x, y, j);
+        int s = 1;
+        while (true){
+            System.out.println("Hello! It`s a calc");
+            Scanner calc = new Scanner(System.in);
+            System.out.println("Input x - first number");
+            int x = calc.nextInt();
+            System.out.println("Input y - second number");
+            int y = calc.nextInt();
+            System.out.println("Input ~1~, if +, ~2~, if -, ~3~, if *, ~4~, if /");
+            int j = calc.nextInt();
+            System.out.printf("x = %d, y = %d, j = %d \n", x, y, j);
 
-        int a = x+y;
-        int b = x-y;
-        int c = x*y;
-        double d = (double)x / y;
+            int a = x + y;
+            int b = x - y;
+            int c = x * y;
+            double d = (double) x / y;
 
-        switch (j){
-            case 1:
-                System.out.printf("x+y = %d \n", a);
-                break;
-            case 2:
-                System.out.printf("x-y = %d \n", b);
-                break;
-            case 3:
-                System.out.printf("x*y = %d \n", c);
-                break;
-            case 4:
-                System.out.printf("x/y = %.2f \n", d);
-                break;
-            default:
-                System.out.printf("Error \n");
+            switch (j) {
+                case 1:
+                    System.out.printf("x+y = %d \n", a);
+                    break;
+                case 2:
+                    System.out.printf("x-y = %d \n", b);
+                    break;
+                case 3:
+                    System.out.printf("x*y = %d \n", c);
+                    break;
+                case 4:
+                    System.out.printf("x/y = %.2f \n", d);
+                    break;
+                default:
+                    System.out.printf("Error \n");
+            }
+            //System.out.println("Bye");
+            //calc.close();
+            s++;
+            if (s == 5) break;
         }
-        System.out.println("Bye");
-        calc.close();
     }
 }
