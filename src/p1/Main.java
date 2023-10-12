9c97ed7563df7c205f776ea213f7d5108a091aad
OlgaTurkina <olechka967@rambler.ru>
2023-10-12 14:58:47 +0400

    1

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,17 +1,65 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int x = 3;
+        int y = 2;
+        int z = x < y ? x + y : x - y;
+        System.out.println(z);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int i;
+        for(i = 1; i < 10; ++i) {
+            System.out.println("Hello");
         }
+
+        for(i = 1; i < 10; ++i) {
+            System.out.println("" + i + " " + i * i);
+        }
+
+        System.out.println(i);
+        System.out.println("----------");
+        int n = 10;
+        int m = 0;
+
+        int x1;
+        for(x1 = n - 1; m < x1; --x1) {
+            System.out.println(m * x1);
+            ++m;
+        }
+
+        System.out.println("__________");
+
+        for(m = 10; m > 0; --m) {
+            System.out.println(m);
+        }
+
+        x1 = 0;
+
+        do {
+            System.out.println("Hello");
+            ++x1;
+        } while(x1 != 5);
+
+        int z1 = 7;
+
+        do {
+            System.out.println(z1);
+            --z1;
+        } while(z1 > 0);
+
+        z1 = -1;
+
+        do {
+            System.out.println(z1);
+            --z1;
+        } while(z1 > 0);
+
+        for(int w = 0; w < 10; ++w) {
+            if (w != 5) {
+                System.out.println(w);
+            }
+        }
+
     }
 }
